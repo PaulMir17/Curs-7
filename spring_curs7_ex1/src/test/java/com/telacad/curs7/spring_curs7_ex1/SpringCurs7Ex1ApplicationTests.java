0282d577ba005027a99b131dc7ccdb1691132aea
PaulMir17 <miricapaul17@gmail.com>
2024-07-18 14:42:00 +0300

    Add files via upload

---
+++ spring_curs7_ex1/src/test/java/com/telacad/curs7/spring_curs7_ex1/SpringCurs7Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telacad.curs7.spring_curs7_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCurs7Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
